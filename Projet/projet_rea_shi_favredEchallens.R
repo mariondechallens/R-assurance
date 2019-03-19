746d60e048e50c226f8a42401a1a3a0f6ec095f4
Marion FDE <marion.favre-d-echallens@student.ecp.fr>
2019-03-19 20:06:08 +0100

    Update projet_rea_shi_favredEchallens.R

---
+++ Projet/projet_rea_shi_favredEchallens.R
@@ -0,0 +1,7 @@
+#### Chargement des données ----  
+
+dir = "C:/Users/Admin/Documents/Centrale Paris/3A/OMA/Réassurance/Examen_2019/Sujet 1/"
+setwd(dir)
+loss = read.csv2('Loss.csv')
+prof = read.csv2('Profile.csv')
+endo = read.csv2('Endorsement.csv')
